0e380f253da1a4338633524e990d1d5575df3f5b
Apurwa-Sinha <155532263+Apurwa-Sinha@users.noreply.github.com>
2026-07-27 13:53:09 +0530

    Refactor userSignupValidator to use express-validator

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-exports.userSignupValidator = (req, res, next) => {
-    req.check('name', 'Name is required').notEmpty();
-    req
-      .check('email', 'Email must be between 3 to 32 characters')
-      .matches(/.+\@.+\..+/)
-      .withMessage('Email must contain @')
-      .isLength({
-        min: 4,
-        max: 32,
-      });
-    req.check('password', 'Password is required').notEmpty();
-    req
-      .check('password')
-      .isLength({ min: 6 })
-      .withMessage('Password must contain at least 6 characters')
-      .matches(/\d/)
-      .withMessage('Password must contain a number');
-    const errors = req.validationErrors();
-    if (errors) {
-      const firstError = errors.map((error) => error.msg)[0];
+const { check, validationResult } = require('express-validator');
+
+exports.userSignupValidator = [
+  check('name', 'Name is required').notEmpty(),
+  check('email', 'Email must be between 4 to 32 characters')
+    .matches(/.+\@.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({
+      min: 4,
+      max: 32,
+    }),
+  check('password', 'Password is required').notEmpty(),
+  check('password')
+    .isLength({ min: 6, max: 72 })
+    .withMessage('Password must be between 6 and 72 characters')
+    .matches(/\d/)
+    .withMessage('Password must contain a number'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const firstError = errors.array().map((error) => error.msg)[0];
       return res.status(400).json({ error: firstError });
     }
     next();
-  };
+  },
+];
