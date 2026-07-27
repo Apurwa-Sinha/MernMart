6fc78a88f29973a579dec1bc2d3c93341c8a3ecc
Apurwa-Sinha <155532263+Apurwa-Sinha@users.noreply.github.com>
2026-07-27 13:56:56 +0530

    Update order.js

--- order.js
+++ order.js
@@ -1,63 +1,73 @@
 const { Order, CartItem } = require('../models/order');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-exports.orderById = (req, res, next, id) => {
-  Order.findById(id)
-    .populate('products.product', 'name price')
-    .exec((err, order) => {
-      if (err || !order) {
-        return res.status(400).json({
-          error: errorHandler(err),
-        });
-      }
-      req.order = order;
-      next();
-    });
-};
-
-exports.create = (req, res) => {
-  // console.log('CREATE ORDER: ', req.body);
-  req.body.order.user = req.profile;
-  const order = new Order(req.body.order);
-  order.save((error, data) => {
-    if (error) {
+exports.orderById = async (req, res, next, id) => {
+  try {
+    const order = await Order.findById(id).populate(
+      'products.product',
+      'name price'
+    );
+    if (!order) {
       return res.status(400).json({
-        error: errorHandler(error),
+        error: "Order doesn't exist",
       });
     }
-    res.json(data);
-  });
+    req.order = order;
+    next();
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err),
+    });
+  }
 };
 
-exports.listOrders = (req, res) => {
-  Order.find()
-    .populate('user', '_id name address')
-    .sort('-created')
-    .exec((err, orders) => {
-      if (err) {
-        return res.status(400).json({
-          error: errorHandler(error),
-        });
-      }
-      res.json(orders);
+exports.create = async (req, res) => {
+  try {
+    req.body.order.user = req.profile._id;
+    const order = new Order(req.body.order);
+    const data = await order.save();
+    res.json(data);
+  } catch (error) {
+    return res.status(400).json({
+      error: errorHandler(error),
     });
+  }
+};
+
+exports.listOrders = async (req, res) => {
+  try {
+    const orders = await Order.find()
+      .populate('user', '_id name address')
+      .sort('-created');
+    res.json(orders);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err),
+    });
+  }
 };
 
 exports.getStatusValues = (req, res) => {
   res.json(Order.schema.path('status').enumValues);
 };
 
-exports.updateOrderStatus = (req, res) => {
-  Order.update(
-    { _id: req.body.orderId },
-    { $set: { status: req.body.status } },
-    (err, order) => {
-      if (err) {
-        return res.status(400).json({
-          error: errorHandler(err),
-        });
-      }
-      res.json(order);
+exports.updateOrderStatus = async (req, res) => {
+  try {
+    const order = await Order.findByIdAndUpdate(
+      req.body.orderId,
+      { $set: { status: req.body.status } },
+      { new: true }
+    );
+    if (!order) {
+      return res.status(400).json({
+        error: "Order doesn't exist",
+      });
     }
-  );
+    res.json(order);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err),
+    });
+  }
 };
+  
