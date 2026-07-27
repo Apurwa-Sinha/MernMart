e3a0285a4f3dae3b1957a80639a9b5f190c57265
Apurwa-Sinha <155532263+Apurwa-Sinha@users.noreply.github.com>
2026-07-27 14:21:21 +0530

    Refactor user.js to use async/await

--- user.js
+++ user.js
@@ -1,17 +1,23 @@
+
 const User = require('../models/user');
 const { Order } = require('../models/order');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-exports.userById = (req, res, next, id) => {
-  User.findById(id).exec((err, user) => {
-    if (err || !user) {
+exports.userById = async (req, res, next, id) => {
+  try {
+    const user = await User.findById(id);
+    if (!user) {
       return res.status(400).json({
         error: 'User not found',
       });
     }
     req.profile = user;
     next();
-  });
+  } catch (err) {
+    return res.status(400).json({
+      error: 'User not found',
+    });
+  }
 };
 
 exports.read = (req, res) => {
@@ -20,108 +26,84 @@ exports.read = (req, res) => {
   return res.json(req.profile);
 };
 
-exports.update = (req, res) => {
-  // console.log('user update', req.body);
-  // req.body.role = 0; // role will always be 0
-  User.findOneAndUpdate(
-    { _id: req.profile._id },
-    { $set: req.body },
-    { new: true },
-    (err, user) => {
-      if (err) {
-        return res.status(400).json({
-          error: 'You are not authorized to perform this action',
-        });
-      }
-      user.hashed_password = undefined;
-      user.salt = undefined;
-      res.json(user);
+exports.update = async (req, res) => {
+  try {
+    // Only allow specific fields to be updated. Spreading req.body directly
+    // into $set would let a client set arbitrary fields (e.g. role, salt,
+    // hashed_password), which is a privilege-escalation risk.
+    const { name, password } = req.body;
+
+    const user = req.profile;
+
+    if (name) {
+      user.name = name;
     }
-  );
-};
 
-// exports.update = (req, res) => {
-//   // console.log('UPDATE USER - req.user', req.user, 'UPDATE DATA', req.body);
-//   const { name, password } = req.body;
-
-//   User.findOne({ _id: req.profile._id }, (err, user) => {
-//     if (err || !user) {
-//       return res.status(400).json({
-//         error: 'User not found',
-//       });
-//     }
-//     if (!name) {
-//       return res.status(400).json({
-//         error: 'Name is required',
-//       });
-//     } else {
-//       user.name = name;
-//     }
-
-//     if (password) {
-//       if (password.length < 6) {
-//         return res.status(400).json({
-//           error: 'Password should be min 6 characters long',
-//         });
-//       } else {
-//         user.password = password;
-//       }
-//     }
-
-//     user.save((err, updatedUser) => {
-//       if (err) {
-//         console.log('USER UPDATE ERROR', err);
-//         return res.status(400).json({
-//           error: 'User update failed',
-//         });
-//       }
-//       updatedUser.hashed_password = undefined;
-//       updatedUser.salt = undefined;
-//       res.json(updatedUser);
-//     });
-//   });
-// };
-
-exports.addOrderToUserHistory = (req, res, next) => {
-  let history = [];
-
-  req.body.order.products.forEach((item) => {
-    history.push({
-      _id: item._id,
-      name: item.name,
-      description: item.description,
-      category: item.category,
-      quantity: item.count,
-      transaction_id: req.body.order.transaction_id,
-      amount: req.body.order.amount,
-    });
-  });
-
-  User.findOneAndUpdate(
-    { _id: req.profile._id },
-    { $push: { history: history } },
-    { new: true },
-    (error, data) => {
-      if (error) {
+    if (password) {
+      if (password.length < 6) {
         return res.status(400).json({
-          error: 'Could not update user purchase history',
+          error: 'Password should be min 6 characters long',
         });
       }
-      next();
+      // assumes User model has a virtual `password` setter that hashes
+      // into hashed_password/salt on save
+      user.password = password;
     }
-  );
+
+    const updatedUser = await user.save();
+    updatedUser.hashed_password = undefined;
+    updatedUser.salt = undefined;
+    res.json(updatedUser);
+  } catch (err) {
+    console.log('USER UPDATE ERROR', err);
+    return res.status(400).json({
+      error: errorHandler(err),
+    });
+  }
 };
 
-exports.purchaseHistory = (req, res) => {
-  Order.find({ user: req.profile._id })
-    .populate('user', '_id name')
-    .sort('-created')
-    .exec((err, orders) => {
-      if (err) {
-        return res.status(400).json({
-          error: errorHandler(err),
-        });
-      }
-      res.json(orders);
+exports.addOrderToUserHistory = async (req, res, next) => {
+  try {
+    let history = [];
+    req.body.order.products.forEach((item) => {
+      history.push({
+        _id: item._id,
+        name: item.name,
+        description: item.description,
+        category: item.category,
+        quantity: item.count,
+        transaction_id: req.body.order.transaction_id,
+        amount: req.body.order.amount,
+      });
     });
+
+    await User.findOneAndUpdate(
+      { _id: req.profile._id },
+      { $push: { history: { $each: history } } },
+      { new: true }
+    );
+
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      error: 'Could not update user purchase history',
+    });
+  }
 };
+
+exports.purchaseHistory = async (req, res) => {
+  try {
+    const orders = await Order.find({ user: req.profile._id })
+      .populate('user', '_id name')
+      .sort('-created');
+    res.json(orders);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler(err),
+    });
+  }
+};
+
+
+
+
