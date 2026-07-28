59197508bcacc3cc7442c8b72712b60d0ddc3d67
Apurwa-Sinha <155532263+Apurwa-Sinha@users.noreply.github.com>
2026-07-28 11:52:51 +0530

    Add user signup and signin validators

---
+++ Validator.js
@@ -0,0 +1,48 @@
+const { check, validationResult } = require('express-validator');
+
+exports.userSignupValidator = [
+  check('name', 'Name is required').notEmpty(),
+  check('email', 'Email must be between 4 to 32 characters')
+    .matches(/.+\@.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({
+      min: 4,
+      max: 32,
+    }),
+  check('password', 'Password is required').notEmpty(),
+  check('password')
+    .isLength({ min: 6, max: 72 })
+    .withMessage('Password must be between 6 and 72 characters')
+    .matches(/\d/)
+    .withMessage('Password must contain a number'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const firstError = errors.array().map((error) => error.msg)[0];
+      return res.status(400).json({ error: firstError });
+    }
+    next();
+  },
+];
+
+exports.userSigninValidator = [
+  check('email', 'Email is required').notEmpty(),
+  check('email', 'Email must be between 4 to 32 characters')
+    .matches(/.+\@.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({
+      min: 4,
+      max: 32,
+    }),
+  check('password', 'Password is required').notEmpty(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const firstError = errors.array().map((error) => error.msg)[0];
+      return res.status(400).json({ error: firstError });
+    }
+    next();
+  },
+];
